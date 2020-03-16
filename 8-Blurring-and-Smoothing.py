515325c125c1e9fc6a2b5b93bd3eedc3f14a7c49
Ahmadreza-n <ahmadrezaanazari@gmail.com>
2020-03-16 18:12:43 +0330

    Session 8: Blurring

--- 8-Blurring-and-Smoothing.py
+++ 8-Blurring-and-Smoothing.py
@@ -16,9 +16,22 @@ while True:
   mask = cv2.inRange(hsv, lower_red, upper_red)
   res = cv2.bitwise_and(frame, frame, mask=mask)
 
-  cv2.imshow('frame', frame)
-  cv2.imshow('mask', mask)
-  cv2.imshow('res', res)
+  kernel = np.ones((15, 15), np.float32) / 255
+  smoothed = cv2.filter2D(res, -1, kernel)
+
+  blur = cv2.GaussianBlur(res, (15, 15), 0)
+
+  median = cv2.medianBlur(res, 15)
+
+  bilateral = cv2.bilateralFilter(res, 15, 75, 75)
+
+  cv2.imshow('Frame', frame)
+  cv2.imshow('Mask', mask)
+  cv2.imshow('Res', res)
+  cv2.imshow('Smoothed', smoothed)
+  cv2.imshow('Blur', blur)
+  cv2.imshow('Median', median)
+  cv2.imshow('Bilateral', bilateral)
 
   k = cv2.waitKey(5) & 0xFF
   if k == 27:
@@ -26,3 +39,4 @@ while True:
 
 cv2.destroyAllWindows()
 cap.release()
+
