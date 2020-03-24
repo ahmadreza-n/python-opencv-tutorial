79e40b72910f8d82e4ea540d7b9b1c6712807050
Ahmadreza-n <ahmadrezaanazari@gmail.com>
2020-03-24 20:20:58 +0430

    Session 11: Template Matching

---
+++ 11-Template-Matching.py
@@ -0,0 +1,24 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import cv2
+import numpy as np
+
+img_bgr = cv2.imread('./www/template-matching.jpg')
+img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+
+template_bgr = cv2.imread('./www/template-for-matching.jpg')
+template_gray = cv2.cvtColor(template_bgr, cv2.COLOR_BGR2GRAY)
+w, h, _ = template_bgr.shape
+
+res = cv2.matchTemplate(img_gray, template_gray, cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where(res >= threshold)
+
+for pt in zip(*loc[::-1]):
+  cv2.rectangle(img_bgr, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+
+cv2.imshow('Detected', img_bgr)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
